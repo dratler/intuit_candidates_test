da46ea9ba58e6e884ca642a74f4b85cc8f34be9d
Shay Dratler <Shay_Dratler@intuit.com>
2019-12-11 23:59:53 +0200

    first commit

---
+++ src/server/index.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const app = express();
+
+app.get('/', function (req, res) {
+  res.json({
+    status: 'Working'
+  });
+});
+
+app.get('/transctions', function (req, res) {
+  res.json({
+    transctions: [{
+        id: 21323424353,
+        amount: 12.45,
+        currency: 'USD'
+      },
+      {
+        id: 6342746827642876,
+        amount: -67.13,
+        currency: "GBP"
+      },
+      {
+        id: 58958088504,
+        amount: 987.5,
+        currency: "NIS"
+      }
+    ]
+  });
+});
+
+app.listen(3000);
